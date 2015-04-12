74ca56681c06b69278a403a4e3db8225135715e6
Wei Chen <ipondering.weic@gmail.com>
2015-04-12 16:16:49 -0700

    Update codatlas.js

--- codatlas.js
+++ codatlas.js
@@ -174,120 +174,131 @@ exports.EdgeKind = EdgeKind;
 exports.process = apply_;
 
 },{"./MetaDataEnum_types.js":1,"underscore":6}],3:[function(require,module,exports){
-serverUri = "ws://www.codatlas.com:9020/socket"
-
-
+var serverUri = "ws://www.codatlas.com:9020/socket"
 var SourcePageDecorator = require("./SourcepageDecorator.js");
 var MetadataProcessor = require("./MetadataProcessor.js");
 var $ = require("jquery");
 
-    // extract plain text from a code block
-    function extractLine (a) {
-        return $(a).text();
-    }
+// extract plain text from a code block
+function extractLine (a) {
+    return $(a).text();
+}
 
-    // Every connection protocol can be plugged in by implementing
-    // request(blocks, successFunction).
-    function request(blocks, onSuccessFunction) {
-        ws = new WebSocket(serverUri);
+// Every connection protocol can be plugged in by implementing
+// request(blocks, successFunction).
+function request(blocks, onSuccessFunction) {
+    ws = new WebSocket(serverUri);
 
-        ws.onopen = function() {
-            var req = [];
-            $.each(blocks, function(i, b) {
-                req.push(extractLine(b));
-            });
-            ws.send(JSON.stringify(req));
-        };
-
-        ws.onmessage = function(ev) {
-            var blockMetaDataList = JSON.parse(ev.data);
-            var res = [];
-            // append meta data to raw data
-            $.each(blocks, function(i, block) {
-                blockMetaData = blockMetaDataList[i];
-                res.push({
-                        "block": block,
-                        "meta_data": blockMetaData
-                });
-            });
-            onSuccessFunction(res);
-            ws.close();
-        };
-
-        ws.onclose = function() {
-            console.log("closing websocket");
-        };
-    }
-
-    // filter too long ot too short lines
-    function filterCodeBlocks (blocks) {
-        var res = [];
-        blocks.each(function(a, d) {
-            var line = extractLine($(d));
-            line.length > 50 && line.length < 1e4 && res.push(d);
+    ws.onopen = function() {
+        var req = [];
+        $.each(blocks, function(i, b) {
+            req.push(extractLine(b));
         });
-        return res;
-    }
-
-    // collect potential HTML blocks with code
-    function gatherCodeBlocks() {
-        var raw_blocks =
-                $("pre, code, .CodeMirror-lines, .dp-highlighter, .syntaxhighlighter")
-                .filter(":visible")
-                .filter(":not(pre *)")
-                .filter(":not(.CodeMirror-lines *)")
-                .filter(":not(.syntaxhighlighter *)")
-                .filter(":not(.dp-highlighter *)");
-        return filterCodeBlocks(raw_blocks);
-    }
-
-    var CODATLAS_DEFINITION_NODE_CLASSNAME = "ca-definition";
-
-    var parseNodeClassName_ = function(kind) {
-            if (kind == NodeKind.USAGE) return CODATLAS_USAGE_NODE_CLASSNAME;
-            else return CODATLAS_DEFINITION_NODE_CLASSNAME;
+        ws.send(JSON.stringify(req));
     };
 
-    // apply analyzed metadata to a DOM element.
-    function applyDecoration(i, decoratedBlock) {
-        var block = decoratedBlock["block"];
-        var metaData = decoratedBlock["meta_data"];
-
-        metaDataDocument = MetadataProcessor.process(metaData);
-
-        nodeClickFuncMk = function (id) {
-            return function() {
-                window.open("http://www.codatlas.com/gotoSymbol/" + id);
-            }
-        };
-
-        if (metaDataDocument.nodes) {
-            $.each(metaDataDocument.nodes, function (i, node) {
-                eventMap = {};
-                eventMap.click = nodeClickFuncMk(node.signature);
-
-                SourcePageDecorator.addSpanToDomTree(block, node, parseNodeClassName_(node.kind), eventMap);
+    ws.onmessage = function(ev) {
+        var blockMetaDataList = JSON.parse(ev.data);
+        var res = [];
+        // append meta data to raw data
+        $.each(blocks, function(i, block) {
+            blockMetaData = blockMetaDataList[i];
+            res.push({
+                "block": block,
+                "meta_data": blockMetaData
             });
+        });
+        onSuccessFunction(res);
+        ws.close();
+    };
+
+    ws.onclose = function() {
+        console.log("closing websocket");
+    };
+}
+
+// filter too long ot too short lines
+function filterCodeBlocks (blocks) {
+    var res = [];
+    blocks.each(function(a, d) {
+        var line = extractLine($(d));
+        line.length > 50 && line.length < 1e4 && res.push(d);
+    });
+    return res;
+}
+
+// collect potential HTML blocks with code
+function gatherCodeBlocks() {
+    var raw_blocks =
+        $("pre, code, .CodeMirror-lines, .dp-highlighter, .syntaxhighlighter")
+        .filter(":visible")
+        .filter(":not(pre *)")
+        .filter(":not(.CodeMirror-lines *)")
+        .filter(":not(.syntaxhighlighter *)")
+        .filter(":not(.dp-highlighter *)");
+    return filterCodeBlocks(raw_blocks);
+}
+
+var CODATLAS_USAGE_NODE_CLASSNAME = "ca-usage";
+var CODATLAS_DEFINITION_NODE_CLASSNAME = "ca-definition";
+
+var parseNodeClassName_ = function(kind) {
+    if (kind == NodeKind.USAGE) return CODATLAS_USAGE_NODE_CLASSNAME;
+    else return CODATLAS_DEFINITION_NODE_CLASSNAME;
+};
+
+// apply analyzed metadata to a DOM element.
+function applyDecoration(i, decoratedBlock) {
+    function buildSymbolTbl(edges) {
+        var symbolTbl = {};
+        $.each(edges, function(i, edge) {
+            symbolTbl[edge.start_id] = edge.end_id;
+        });
+        return symbolTbl;
+    }
+    
+    var block = decoratedBlock["block"];
+    var metaData = decoratedBlock["meta_data"];
+
+    metaDataDocument = MetadataProcessor.process(metaData);
+
+    nodeClickFuncMk = function (id) {
+        return function() {
+            window.open("http://www.codatlas.com/gotoNode/" + encodeURIComponent(id));
         }
+    };
+
+    if (metaDataDocument.nodes) {
+        var symbolTbl = buildSymbolTbl(metaDataDocument.edges);
+        $.each(metaDataDocument.nodes, function (i, node) {
+            eventMap = {};
+            if (symbolTbl.hasOwnProperty(node.signature)) {
+                eventMap.click = nodeClickFuncMk(symbolTbl[node.signature]);
+            } else {
+                eventMap.click = nodeClickFuncMk(node.signature);
+            }            
+
+            SourcePageDecorator.addSpanToDomTree(block, node, parseNodeClassName_(node.kind), eventMap);
+        });
     }
+}
 
-    // ask server for meta of the code blocks
-    function analyzeCodeBlocks(onSuccessFunction) {
-        var blocks = gatherCodeBlocks();
-        request(blocks, onSuccessFunction);
-    }
+// ask server for meta of the code blocks
+function analyzeCodeBlocks(onSuccessFunction) {
+    var blocks = gatherCodeBlocks();
+    request(blocks, onSuccessFunction);
+}
 
-    // decorate meta to existing node
-    function annoteCodeBlocks(annotatedBlocks) {
-        $.each(annotatedBlocks, applyDecoration);
-    }
+// decorate meta to existing node
+function annoteCodeBlocks(annotatedBlocks) {
+    $.each(annotatedBlocks, applyDecoration);
+}
 
 
-    function run_snippet() {
-        analyzeCodeBlocks(annoteCodeBlocks);
-    }
-    window.onload = run_snippet;
-
+function run_snippet() {
+    analyzeCodeBlocks(annoteCodeBlocks);
+}
+window.onload = run_snippet;
 
 },{"./MetadataProcessor.js":2,"./SourcepageDecorator.js":4,"jquery":5}],4:[function(require,module,exports){
   var _ = require("underscore");
@@ -382,8 +393,6 @@ var $ = require("jquery");
   // compute the intersection of two range, if two range has no intersection, -1 will be returned if lambdaRange is to
   // the left of spanRange and 1 if the opposite
   var intersectRange_ = function(lambdaRange, spanRange) {
-    console.log("lambda range : %o", lambdaRange );
-    console.log("span range: %o", spanRange);
     if (lambdaRange.end <= spanRange.start) {
       return -1;
     } else if (lambdaRange.start >= spanRange.end) {
